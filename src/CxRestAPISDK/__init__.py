4563d2d7cadd59c14a527dca6791652759cc099a
Happy Yang <happy.yang@checkmarx.com>
2019-08-28 13:59:29 +0800

    add DataRententionAPI and EnginesAPI

--- src/CxRestAPISDK/__init__.py
+++ src/CxRestAPISDK/__init__.py
@@ -6,5 +6,7 @@ from .sast.projects.ProjectsAPI import ProjectsAPI
 from .sast.projects.CustomTasksAPI import CustomTasksAPI
 from .sast.projects.CustomFieldsAPI import CustomFieldsAPI
 from .sast.scans.ScansAPI import ScansAPI
+from .sast.dataRetention.DataRetentionAPI import DataRetentionAPI
+from .sast.engines.EnginesAPI import EnginesAPI
 from .osa.OsaAPI import OsaAPI
 from .exceptions.CxError import (BadRequestError, NotFoundError, UnknownHttpStatusError)
