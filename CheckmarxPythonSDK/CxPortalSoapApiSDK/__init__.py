419f497d91d1885f567c6708c7776c62e319daaf
Happy Yang <happy.yang@checkmarx.com>
2020-11-02 20:20:30 +0800

    expose api directly for package CxPortalSoapApiSDK

---
+++ CheckmarxPythonSDK/CxPortalSoapApiSDK/__init__.py
@@ -0,0 +1,12 @@
+from .CxPortalWebService import (
+    activate_saas_user,
+    add_license_expiration_notification,
+    create_new_preset,
+    delete_preset,
+    get_preset_list,
+    get_server_license_data,
+    get_server_license_summary,
+    delete_project,
+    delete_projects,
+    get_version_number
+)
--- tests/test_cx_portal_web_service.py
+++ tests/test_cx_portal_web_service.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-from CheckmarxPythonSDK.CxPortalSoapApiSDK.CxPortalWebService import (
+from CheckmarxPythonSDK.CxPortalSoapApiSDK import (
     activate_saas_user, add_license_expiration_notification,
     create_new_preset, delete_preset, get_preset_list, get_server_license_data, get_server_license_summary,
     delete_project, delete_projects, get_version_number
