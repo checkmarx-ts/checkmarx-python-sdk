f60229f7ac218a656bfa6d5688eb11aadb0fe8cb
Happy Yang <happy.yang@checkmarx.com>
2020-11-02 12:45:07 +0800

    [Modify] get_new_token to accept all variables

--- CheckmarxPythonSDK/auth.py
+++ CheckmarxPythonSDK/auth.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 import requests
 
-from .config import config
 from .compat import OK
 
 from urllib3 import disable_warnings
@@ -10,26 +9,31 @@ from urllib3.exceptions import InsecureRequestWarning
 disable_warnings(InsecureRequestWarning)
 
 
-def get_new_token(username=config.get("username"), password=config.get("password")):
+def get_new_token(base_url, username, password, grant_type, scope, client_id, client_secret):
     """
 
     Args:
-        username (str):
-        password (str):
+        base_url (str): "ttp://localhost"
+        username (str): ***
+        password (str): ***
+        grant_type (str): "password"
+        scope (str): "sast_api"
+        client_id (str): "resource_owner_client" or "resource_owner_sast_client"
+        client_secret (str): "014DF517-39D1-4453-B7B3-9930C563627C"
 
     Returns:
         Bear Token (str)
     """
 
-    token_url = config.get("base_url") + "/cxrestapi/auth/identity/connect/token"
+    token_url = base_url + "/cxrestapi/auth/identity/connect/token"
 
     req_data = {
         "username": username,
         "password": password,
-        "grant_type": "password",
-        "scope": "sast_api",
-        "client_id": "resource_owner_sast_client",
-        "client_secret": "014DF517-39D1-4453-B7B3-9930C563627C"
+        "grant_type": grant_type,
+        "scope": scope,
+        "client_id": client_id,
+        "client_secret": client_secret
     }
 
     response = requests.post(url=token_url, data=req_data, verify=False)
