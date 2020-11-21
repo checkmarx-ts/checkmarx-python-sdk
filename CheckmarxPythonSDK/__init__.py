c534e1d3db679c13e56941ba51fc2f634b4ea0e7
Happy Yang <happy.yang@checkmarx.com>
2020-11-21 18:52:35 +0800

    version

--- CheckmarxPythonSDK/__init__.py
+++ CheckmarxPythonSDK/__init__.py
@@ -1,4 +1,4 @@
 # encoding: utf-8
 
 __author__ = 'Checkmarx'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", 'UTF-8') as fh:
 
 setup(
     name="CheckmarxPythonSDK",
-    version="0.1.2",
+    version="0.1.3",
     author="Happy Yang",
     author_email="happy.yang@checkmarx.com",
     description="Checkmarx Python SDK",
