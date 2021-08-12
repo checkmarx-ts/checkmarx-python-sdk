172b6210aeff54a989f40999f3de205bf41f5758
Happy Yang <happy.yang@checkmarx.com>
2021-08-12 17:50:50 +0800

    config

--- CheckmarxPythonSDK/config.py
+++ CheckmarxPythonSDK/config.py
@@ -4,9 +4,11 @@ from requests.compat import is_py2
 
 if is_py2:
     import ConfigParser
+
     configparser = ConfigParser
 else:
     import configparser
+
     configparser = configparser
 
 from optparse import OptionParser, SUPPRESS_HELP, BadOptionError, AmbiguousOptionError
@@ -61,33 +63,33 @@ def get_config_info_from_config_file():
     cxsast_config = None
     if parser_obj.has_section("checkmarx"):
         cxsast_config = {
-            "base_url": parser_obj.get("checkmarx", "base_url", fallback=None),
-            "username": parser_obj.get("checkmarx", "username", fallback=None),
-            "password": parser_obj.get("checkmarx", "password", fallback=None),
-            "grant_type": parser_obj.get("checkmarx", "grant_type", fallback=None),
-            "scope": parser_obj.get("checkmarx", "scope", fallback=None),
-            "client_id": parser_obj.get("checkmarx", "client_id", fallback=None),
-            "client_secret": parser_obj.get("checkmarx", "client_secret", fallback=None),
-            "scan_preset": parser_obj.get("checkmarx", "scan_preset", fallback=None),
-            "configuration": parser_obj.get("checkmarx", "configuration", fallback=None),
-            "team_full_name": parser_obj.get("checkmarx", "team_full_name", fallback=None),
-            "max_try": parser_obj.getint("checkmarx", "max_try", fallback=3),
-            "report_folder": parser_obj.get("checkmarx", "report_folder", fallback=None),
+            "base_url": parser_obj.get("checkmarx", "base_url"),
+            "username": parser_obj.get("checkmarx", "username"),
+            "password": parser_obj.get("checkmarx", "password"),
+            "grant_type": parser_obj.get("checkmarx", "grant_type"),
+            "scope": parser_obj.get("checkmarx", "scope"),
+            "client_id": parser_obj.get("checkmarx", "client_id"),
+            "client_secret": parser_obj.get("checkmarx", "client_secret"),
+            "scan_preset": parser_obj.get("checkmarx", "scan_preset"),
+            "configuration": parser_obj.get("checkmarx", "configuration"),
+            "team_full_name": parser_obj.get("checkmarx", "team_full_name"),
+            "max_try": parser_obj.getint("checkmarx", "max_try"),
+            "report_folder": parser_obj.get("checkmarx", "report_folder"),
         }
 
     cxsca_config = None
     if parser_obj.has_section("CxSCA"):
         cxsca_config = {
-            "access_control_url": parser_obj.get("CxSCA", "access_control_url", fallback=None),
-            "server": parser_obj.get("CxSCA", "server", fallback=None),
-            "account": parser_obj.get("CxSCA", "account", fallback=None),
-            "username": parser_obj.get("CxSCA", "username", fallback=None),
-            "password": parser_obj.get("CxSCA", "password", fallback=None),
+            "access_control_url": parser_obj.get("CxSCA", "access_control_url"),
+            "server": parser_obj.get("CxSCA", "server"),
+            "account": parser_obj.get("CxSCA", "account"),
+            "username": parser_obj.get("CxSCA", "username"),
+            "password": parser_obj.get("CxSCA", "password"),
         }
 
     return {
         "CxSAST": cxsast_config,
-        "CxSCA": cxsca_config
+        "CxSCA": cxsca_config,
     }
 
 
@@ -102,28 +104,32 @@ def get_config_info_from_environment_variables():
     if max_try:
         max_try = int(max_try)
 
+    cxsast_config = {
+        "base_url": os.getenv("cxsast_base_url"),
+        "username": os.getenv("cxsast_username"),
+        "password": os.getenv("cxsast_password"),
+        "grant_type": os.getenv("cxsast_grant_type"),
+        "scope": os.getenv("cxsast_scope"),
+        "client_id": os.getenv("cxsast_client_id"),
+        "client_secret": os.getenv("cxsast_client_secret"),
+        "scan_preset": os.getenv("cxsast_scan_preset"),
+        "configuration": os.getenv("cxsast_configuration"),
+        "team_full_name": os.getenv("cxsast_team_full_name"),
+        "max_try": max_try,
+        "report_folder": os.getenv("cxsast_report_folder"),
+    }
+
+    cxsca_config = {
+        "access_control_url": os.getenv("cxsca_access_control_url"),
+        "server": os.getenv("cxsca_server"),
+        "account": os.getenv("cxsca_account"),
+        "username": os.getenv("cxsca_username"),
+        "password": os.getenv("cxsca_password"),
+    }
+
     return {
-        "CxSAST": {
-            "base_url": os.getenv("cxsast_base_url"),
-            "username": os.getenv("cxsast_username"),
-            "password": os.getenv("cxsast_password"),
-            "grant_type": os.getenv("cxsast_grant_type"),
-            "scope": os.getenv("cxsast_scope"),
-            "client_id": os.getenv("cxsast_client_id"),
-            "client_secret": os.getenv("cxsast_client_secret"),
-            "scan_preset": os.getenv("cxsast_scan_preset"),
-            "configuration": os.getenv("cxsast_configuration"),
-            "team_full_name": os.getenv("cxsast_team_full_name"),
-            "max_try": max_try,
-            "report_folder": os.getenv("cxsast_report_folder"),
-        },
-        "CxSCA": {
-            "access_control_url": os.getenv("cxsca_access_control_url"),
-            "server": os.getenv("cxsca_server"),
-            "account":  os.getenv("cxsca_account"),
-            "username": os.getenv("cxsca_username"),
-            "password": os.getenv("cxsca_password"),
-        }
+        "CxSAST": cxsast_config,
+        "CxSCA": cxsca_config,
     }
 
 
@@ -137,6 +143,7 @@ class PassThroughOptionParser(OptionParser):
     sys.exit(status) will still be called if a known argument is passed
     incorrectly (e.g. missing arguments or bad argument types, etc.)
     """
+
     def _process_args(self, largs, rargs, values):
         while rargs:
             try:
@@ -177,28 +184,32 @@ def get_config_info_from_command_line_arguments():
     if max_try:
         max_try = int(max_try)
 
+    cxsast_config = {
+        "base_url": options.cxsast_base_url,
+        "username": options.cxsast_username,
+        "password": options.cxsast_password,
+        "grant_type": options.cxsast_grant_type,
+        "scope": options.cxsast_scope,
+        "client_id": options.cxsast_client_id,
+        "client_secret": options.cxsast_client_secret,
+        "scan_preset": options.cxsast_scan_preset,
+        "configuration": options.cxsast_configuration,
+        "team_full_name": options.cxsast_team_full_name,
+        "max_try": max_try,
+        "report_folder": options.cxsast_report_folder,
+    }
+
+    cxsca_config = {
+        "access_control_url": options.cxsca_access_control_url,
+        "server": options.cxsca_server,
+        "account": options.cxsca_account,
+        "username": options.cxsca_username,
+        "password": options.cxsca_password,
+    }
+
     return {
-        "CxSAST": {
-            "base_url": options.cxsast_base_url,
-            "username": options.cxsast_username,
-            "password": options.cxsast_password,
-            "grant_type": options.cxsast_grant_type,
-            "scope": options.cxsast_scope,
-            "client_id": options.cxsast_client_id,
-            "client_secret": options.cxsast_client_secret,
-            "scan_preset": options.cxsast_scan_preset,
-            "configuration": options.cxsast_configuration,
-            "team_full_name": options.cxsast_team_full_name,
-            "max_try": max_try,
-            "report_folder": options.cxsast_report_folder,
-        },
-        "CxSCA": {
-            "access_control_url": options.cxsca_access_control_url,
-            "server": options.cxsca_server,
-            "account": options.cxsca_account,
-            "username": options.cxsca_username,
-            "password": options.cxsca_password,
-        }
+        "CxSAST": cxsast_config,
+        "CxSCA": cxsca_config,
     }
 
 
@@ -224,7 +235,7 @@ global_config = {
         "account": None,
         "username": None,
         "password": None,
-    }
+    },
 }
 
 
