1ef236701e8b18b925ea070459011bd6e6d11373
Happy Yang <happy.yang@checkmarx.com>
2022-03-04 12:11:38 +0800

    CxAST test applications api

---
+++ tests/test_ast_applications_api.py
@@ -0,0 +1,77 @@
+from CheckmarxPythonSDK.CxAST import (
+    create_an_application,
+    get_a_list_of_applications,
+    get_application_id_by_name,
+    get_all_application_tags,
+    get_an_application_by_id,
+    update_an_application,
+    delete_an_application,
+    create_an_application_rule,
+    get_a_list_of_rules_for_a_specific_application,
+    get_an_application_rule,
+    update_an_application_rule,
+    delete_an_application_rule,
+)
+
+application_name = "happy-test-application-2022-03-02"
+
+
+def test_create_an_application():
+    application = create_an_application(name=application_name)
+    assert application is not None
+
+
+def test_get_a_list_of_applications():
+    response = get_a_list_of_applications()
+    applications = response.get("applications")
+    assert len(applications) > 1
+
+
+def test_get_application_id_by_name():
+    application_id = get_application_id_by_name(name=application_name)
+    assert application_id is not None
+
+
+def test_get_all_application_tags():
+    all_tags = get_all_application_tags()
+    assert all_tags is not None
+
+
+def test_get_an_application_by_id():
+    application = get_an_application_by_id(application_id='8b5cafa7-44fe-4dff-a713-8344f021fdd1')
+    assert application is not None
+
+
+def test_update_an_application():
+    is_successful = update_an_application(application_id='8b5cafa7-44fe-4dff-a713-8344f021fdd1')
+    assert is_successful is True
+
+
+def test_delete_an_application():
+    application_id = get_application_id_by_name(name=application_name)
+    is_successful = delete_an_application(application_id=application_id)
+    assert is_successful is True
+
+
+def test_create_an_application_rule():
+    application_id = get_application_id_by_name(name=application_name)
+    rule_type = ""
+    rule_value = ""
+    application_rule = create_an_application_rule(application_id, rule_type, rule_value)
+    assert application_rule is not None
+
+
+def test_get_a_list_of_rules_for_a_specific_application():
+    pass
+
+
+def test_get_an_application_rule():
+    pass
+
+
+def test_update_an_application_rule():
+    pass
+
+
+def test_delete_an_application_rule():
+    pass
