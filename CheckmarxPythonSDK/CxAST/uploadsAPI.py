cd8ed3570e922d7555aee79f44041302e96a6eb5
HappyY19 <yang2149@163.com>
2022-04-18 18:47:55 +0800

    CxAST uploadsAPI

--- CheckmarxPythonSDK/CxAST/uploadsAPI.py
+++ CheckmarxPythonSDK/CxAST/uploadsAPI.py
@@ -3,6 +3,8 @@ import requests
 
 from .httpRequests import post_request, put_request
 from ..compat import OK
+from os.path import exists
+from . import authHeaders
 
 
 def create_a_pre_signed_url_to_upload_files():
@@ -34,10 +36,18 @@ def upload_zip_content_for_scanning(upload_link, zip_file_path):
     """
     is_successful = False
 
+    if not zip_file_path or not exists(zip_file_path):
+        print("zip file path: {} does not exist".format(zip_file_path))
+
     url = "{uploadLink}".format(uploadLink=upload_link)
 
+    headers = authHeaders.auth_headers.copy()
+    headers.update(
+        {"Content-Type": "application/x-www-form-urlencoded"}
+    )
+
     with open(zip_file_path, 'rb') as data:
-        response = requests.put(url=url, data=data)
+        response = requests.put(url=url, data=data, headers=headers)
         if response.status_code == OK:
             is_successful = True
 
