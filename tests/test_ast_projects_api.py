4a304a93ea4ae857027958beeb13f1e6be716b96
HappyY19 <yang2149@163.com>
2022-04-19 18:04:56 +0800

    cx_ast projectsAPI

--- tests/test_ast_projects_api.py
+++ tests/test_ast_projects_api.py
@@ -10,38 +10,48 @@ from CheckmarxPythonSDK.CxAST import (
     delete_a_project,
 )
 
+from CheckmarxPythonSDK.CxAST.dto import (
+    ProjectInput
+)
+
 new_project_name = "happy-test-2022-02-28"
 
 
 def test_create_a_project():
-    response = create_a_project(name=new_project_name)
-    assert response is not None
+    project_id = get_project_id_by_name(name=new_project_name)
+    if not project_id:
+        response = create_a_project(
+            project_input=ProjectInput(
+                name=new_project_name
+            )
+        )
+        assert response is not None
 
 
 def test_get_a_list_of_projects():
     response = get_a_list_of_projects()
-    assert len(response.get("projects")) > 1
+    assert len(response.projects) > 1
 
 
 def test_get_a_list_of_projects_with_ids():
     response = get_a_list_of_projects(ids=["a2feaa26-a515-4716-a2c5-b39c784980fb",
                                            "98551291-5aa8-4322-95c3-acd5a23c8e65"])
-    assert len(response.get("projects")) == 2
+    assert len(response.projects) == 2
 
 
 def test_get_a_list_of_project_with_names():
     response = get_a_list_of_projects(names=["JS_courselit", "hackyodyssey_courselit_zip"])
-    assert len(response.get("projects")) == 2
+    assert len(response.projects) == 2
 
 
 def test_get_a_list_of_project_with_name():
     response = get_a_list_of_projects(name="jenkins")
-    assert len(response.get("projects")) > 1
+    assert len(response.projects) > 1
 
 
 def test_get_a_list_of_project_with_name_regex():
     response = get_a_list_of_projects(name_regex="(?i)jenkins$")
-    assert len(response.get("projects")) == 1
+    assert len(response.projects) == 1
 
 
 def test_get_project_id_by_name():
@@ -61,7 +71,7 @@ def test_get_last_scan_info():
 
 def test_get_last_scan_info_filter_by_project_ids():
     response = get_last_scan_info(project_ids=["a2feaa26-a515-4716-a2c5-b39c784980fb",
-                                           "98551291-5aa8-4322-95c3-acd5a23c8e65"])
+                                               "98551291-5aa8-4322-95c3-acd5a23c8e65"])
     assert response is not None
 
 
@@ -87,15 +97,16 @@ def test_get_branches_filter_by_branch_name():
 
 
 def test_get_a_project_by_id():
-    project = get_a_project_by_id(project_id="00592ce6-ee7c-4738-aaff-115d1f864970")
+    project_id = get_project_id_by_name(name=new_project_name)
+    project = get_a_project_by_id(project_id=project_id)
     assert project is not None
 
 
 def test_update_a_project():
-    project_id = '00592ce6-ee7c-4738-aaff-115d1f864970'
+    project_id = get_project_id_by_name(name=new_project_name)
     is_successful = update_a_project(
         project_id=project_id,
-        repo_url="https://github.com/checkmarx-ts/checkmarx-python-sdk.git"
+        project_input=ProjectInput(repo_url="https://github.com/checkmarx-ts/checkmarx-python-sdk.git")
     )
     assert is_successful is True
 
