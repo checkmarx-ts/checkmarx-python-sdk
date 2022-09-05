78a97134d05e5b802d3b48abf376d196dbfc07aa
HappyY19 <yang2149@163.com>
2022-09-05 11:43:38 +0800

    CxRestAPI/test_team.py

--- tests/CxRestAPI/test_team.py
+++ tests/CxRestAPI/test_team.py
@@ -15,5 +15,7 @@ def test_team():
     assert team_id is not None
     team_full_name = team_api.get_team_full_name_by_team_id(1)
     assert team_full_name == "/CxServer"
-    new_team_id = team_api.create_team("Dagger Team", 1)
-    assert new_team_id > 1
+    team_id = team_api.get_team_id_by_team_full_name("/CxServer/Dagger Team")
+    if not team_id:
+        new_team_id = team_api.create_team("Dagger Team", 1)
+        assert new_team_id > 1
