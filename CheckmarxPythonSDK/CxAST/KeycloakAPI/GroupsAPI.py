d1879fe61e9f71bc9f173d9dfb008af78e2abc50
Jimmy <jimmy-ly00@users.noreply.github.com>
2022-11-25 23:11:56 +0000

    Added group API functions

--- CheckmarxPythonSDK/CxAST/KeycloakAPI/GroupsAPI.py
+++ CheckmarxPythonSDK/CxAST/KeycloakAPI/GroupsAPI.py
@@ -1,3 +1,4 @@
+import json
 from ..httpRequests import get_request
 from ..utilities import get_url_param, type_check
 from .url import api_url
@@ -37,4 +38,63 @@ def get_group_hierarchy(realm, brief_representation=False, first=None, max_resul
     response = response.json()
     groups = [construct_group(item) for item in response]
     return groups
+
+def get_group_by_name(realm, group_name):
+    type_check(realm, str)
+    type_check(group_name, str)
+
+    relative_url = api_url + f"/{realm}/groups"
+    response = get_request(relative_url=relative_url, is_iam=True)
+    response = response.json()
+    groups = [construct_group(item) for item in response]
+
+    for group in groups:
+        if group_name and group.name == group_name:
+            return group.id
+
+def create_group(realm, group_name):
+    type_check(realm, str)
+
+    relative_url = api_url + f"/{realm}/groups"
+    post_data = json.dumps(
+        {
+            'name': group_name 
+        }
+    )
+    response = post_request(relative_url=relative_url, data=post_data, is_iam=True)
+    return response
+
+def create_subgroup(realm, group_id, subgroup_name):
+    type_check(realm, str)
+
+    relative_url = api_url + f"/{realm}/groups/{group_id}/children"
+    post_data = json.dumps(
+        {
+            'name': subgroup_name 
+        }
+    )
+    response = post_request(relative_url=relative_url, data=post_data, is_iam=True)
+    return response
+
+def add_group_role(realm, group_id, container_id, role_id, role_name):
+    type_check(realm, str)
+    type_check(group_id, str)
+    type_check(role_name, str)
+    type_check(role_id, str)
+
+    relative_url = api_url + f"/{realm}/groups/{group_id}/role-mappings/clients/{container_id}" 
+    post_data = json.dumps(
+        [{
+            "clientRole": True,
+            "composite":True,
+            "containerId":f"{container_id}",
+            "description":"Scan projects in Groups of this user",
+            "id":f"{role_id}",
+            "name":f"{role_name}"
+        }]
+    )
+    print(post_data)
+    response = post_request(relative_url=relative_url, data=post_data)
+    return response
+
 
