70886cf46cc140fb78ae2a1d07d6dd774b4753e0
HappyY19 <yang2149@163.com>
2022-12-10 21:07:33 +0800

    keycloakapi

--- CheckmarxPythonSDK/CxOne/KeycloakAPI/__init__.py
+++ CheckmarxPythonSDK/CxOne/KeycloakAPI/__init__.py
@@ -5,5 +5,16 @@ from .UsersAPI import (
 )
 
 from .GroupsAPI import (
-    get_group_hierarchy
+    get_group_hierarchy,
+    create_group_set,
+    get_group_by_name,
+    get_number_of_groups_in_a_realm,
+    get_group_by_id,
+    update_group_by_id,
+    delete_group_by_id,
+    create_subgroup,
+    get_group_permissions,
+    update_group_permissions,
+    get_group_members,
+    create_group,
 )
