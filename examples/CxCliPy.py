5728240b65c5a9921d27d634b27ab38f4e320a7c
HappyY19 <yang2149@163.com>
2023-02-25 17:32:44 +0800

    CxCliPy

--- examples/CxCliPy.py
+++ examples/CxCliPy.py
@@ -7,7 +7,7 @@ Sample usage
 /home/happy/Documents/CxCliPy/dist/CxCliPy scan --cxsast_base_url http://192.168.3.84 --cxsast_username Admin \
 --cxsast_password *** --preset All --incremental False --location_type Folder \
 --location_path /home/happy/Documents/JavaVulnerableLab \
---project_name /CxServer/happy-2022-11-21 --exclude_folders "test" --exclude_files "*min.js" \
+--project_name /CxServer/happy-2022-11-21 --exclude_folders "test,integrationtest" --exclude_files "*min.js" \
 --report_csv cx-report.csv \
 --full_scan_cycle 10
 """
@@ -75,16 +75,94 @@ def get_command_line_arguments():
     return parser.parse_known_args()
 
 
-def create_zip_file_from_location_path(location_path_str: str, project_name: str):
+def group_str_by_wildcard_character(exclusions):
+    """
+
+    Args:
+        exclusions (str): commaseparated string
+        for example, "*.min.js,readme,*.txt,test*,*doc*"
+
+    Returns:
+        dict
+        {
+         "prefix_list": ["test"], # wildcard (*) at end, but not start
+         "suffix_list": [".min.js", ".txt"],  # wildcard (*) at start, but not end
+         "inner_List": ["doc"],   # wildcard (*) at both end and start
+         "word_list": ["readme"]  # no wildcard
+        }
+    """
+    result = {
+        "prefix_list": [],
+        "suffix_list": [],
+        "inner_List": [],
+        "word_list": [],
+    }
+    if not exclusions:
+        return result
+    string_list = exclusions.lower().split(',')
+    string_set = set(string_list)
+    for string in string_set:
+        new_string = string.strip()
+        # ignore any string that with slash or backward slash
+        if '/' in new_string or "\\" in new_string:
+            continue
+        if new_string.endswith("*") and not new_string.startswith("*"):
+            result["prefix_list"].append(new_string.rstrip("*"))
+        elif new_string.startswith("*") and not new_string.endswith("*"):
+            result["suffix_list"].append(new_string.lstrip("*"))
+        elif new_string.endswith("*") and new_string.startswith("*"):
+            result["inner_List"].append(new_string.strip("*"))
+        else:
+            result["word_list"].append(new_string)
+    return result
+
+
+def should_be_excluded(exclusions, target):
+    """
+
+    Args:
+        exclusions (str):
+        target (str):
+
+    Returns:
+
+    """
+    result = False
+    target = target.lower()
+    groups_of_exclusions = group_str_by_wildcard_character(exclusions)
+    if target.startswith(tuple(groups_of_exclusions["prefix_list"])):
+        result = True
+    if target.endswith(tuple(groups_of_exclusions["suffix_list"])):
+        result = True
+    if any([True if inner_text in target else False for inner_text in groups_of_exclusions["inner_List"]]):
+        result = True
+    if target in groups_of_exclusions["word_list"]:
+        result = True
+    return result
+
+
+def create_zip_file_from_location_path(location_path_str: str, project_name: str,
+                                       exclude_folders_str=None, exclude_files_str=None):
     """
 
     Args:
         location_path_str (str):
-        project_name (str)
+        project_name (str):
+        exclude_folders_str (str): comma separated string
+        exclude_files_str (str): comma separated string
 
     Returns:
         str (ZIP file path)
     """
+    exclude_folders = ".*,bin,target,images,Lib,node_modules"
+    exclude_files = ".*,*.min.js"
+    if exclude_folders_str is not None:
+        exclude_folders += ","
+        exclude_folders += exclude_folders_str
+    if exclude_files_str is not None:
+        exclude_files += ","
+        exclude_files += exclude_files_str
+
     from pathlib import Path
     import tempfile
     temp_dir = tempfile.gettempdir()
@@ -96,15 +174,14 @@ def create_zip_file_from_location_path(location_path_str: str, project_name: str
     with ZipFile(file_path, "w", ZIP_DEFLATED) as zip_file:
         root_len = len(location_path_str) + 1
         for base, dirs, files in os.walk(location_path_str):
-            if ".git" in base or ".idea" in base or ".settings" in base or "bin" in base or "target" in base \
-                    or "images" in base:
+            path_folders = base.split(os.sep)
+            if any([should_be_excluded(exclude_folders, folder) for folder in path_folders]):
                 continue
             for file in files:
-                if file.startswith("."):
+                file_lower_case = file.lower()
+                if not file_lower_case.endswith(tuple(extensions)):
                     continue
-                if not file.endswith(tuple(extensions)):
-                    continue
-                if file.endswith(".min.js"):
+                if should_be_excluded(exclude_files, file_lower_case):
                     continue
                 fn = os.path.join(base, file)
                 zip_file.write(fn, fn[root_len:])
@@ -316,7 +393,8 @@ def run_scan_and_generate_reports(arguments):
     project_path_list = project_full_path.split("/")
     project_name = project_path_list[-1]
     team_full_name = "/".join(project_path_list[0: len(project_path_list)-1])
-    zip_file_path = create_zip_file_from_location_path(location_path, project_name)
+    zip_file_path = create_zip_file_from_location_path(location_path, project_name, exclude_folders_str=exclude_folders,
+                                                       exclude_files_str=exclude_files)
     logger.info(f"ZIP file created: {zip_file_path}")
     scan_id = cx_scan_from_local_zip_file(preset_name=preset, team_full_name=team_full_name, project_name=project_name,
                                           zip_file_path=zip_file_path, exclude_folders=exclude_folders,
