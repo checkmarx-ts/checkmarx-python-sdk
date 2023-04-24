4854a1009201f6988185e15d714adee712b6ab77
HappyY19 <yang2149@163.com>
2023-04-24 13:52:00 +0800

    [ADD] test_get_query_id_and_query_version_code

--- tests/CxSAST/CxRestAPI/test_queries_api.py
+++ tests/CxSAST/CxRestAPI/test_queries_api.py
@@ -13,3 +13,11 @@ def test_get_the_full_description_of_the_query():
     query_id = 589
     query_description = query_api.get_the_full_description_of_the_query(query_id)
     assert query_description is not None
+
+
+def test_get_query_id_and_query_version_code():
+    query_api = QueriesAPI()
+    query_id_and_version_code = query_api.get_query_id_and_query_version_code(
+        language="Java", query_name="SQL_Injection", severity="High"
+    )
+    assert query_id_and_version_code is not None
