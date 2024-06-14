1c5322ed360e6d21191f6c47a87c447fad194f6d
unknown <yang2149@163.com>
2024-06-14 15:50:58 +0800

    Add test_get_server_license_data

--- tests/CxSAST/CxRestAPI/test_general_api.py
+++ tests/CxSAST/CxRestAPI/test_general_api.py
@@ -1,6 +1,11 @@
 from CheckmarxPythonSDK.CxRestAPISDK import GeneralAPI
 
 
+def test_get_server_license_data():
+    result = GeneralAPI().get_server_license_data()
+    assert result is not None
+
+
 def test_get_server_system_version():
     result = GeneralAPI().get_server_system_version()
     assert result is not None
