49b5800fe57fde504510485f9d68408b8993a389
happy yang <yang2149@163.com>
2024-07-19 15:04:01 +0800

    [UPDATE] CxOne/auditTrailAPI.py

--- CheckmarxPythonSDK/CxOne/auditTrailAPI.py
+++ CheckmarxPythonSDK/CxOne/auditTrailAPI.py
@@ -1,6 +1,5 @@
-from .httpRequests import get_request, post_request, put_request, delete_request
-from CheckmarxPythonSDK.utilities.compat import NO_CONTENT, CREATED
-from .utilities import get_url_param, type_check, list_member_type_check
+from .httpRequests import get_request
+from .utilities import type_check
 from .dto import (
     AuditEvent,
     AuditEventLink,
