cc163082860f70fc617e9f32fe567afe2de0bf18
happy yang <yang2149@163.com>
2024-10-16 12:54:05 +0800

    [ADD] CxOne scaAPI

--- CheckmarxPythonSDK/CxOne/scaAPI.py
+++ CheckmarxPythonSDK/CxOne/scaAPI.py
@@ -2,11 +2,29 @@ from ..CxScaApiSDK.api import (
     Sca
 )
 from .httpRequests import (
-    get_request as get,
-    post_request as post,
-    put_request as put,
-    delete_request as delete,
+    get_request as get_re,
+    post_request as post_re,
+    put_request as put_re,
+    delete_request as delete_re,
 )
+
+url_prefix = "/api/sca"
+
+
+def get(relative_url):
+    return get_re(relative_url=url_prefix + relative_url)
+
+
+def post(relative_url, data, files=None):
+    return post_re(relative_url=url_prefix + relative_url, data=data, files=files)
+
+
+def put(relative_url, data, files=None):
+    return put_re(relative_url=url_prefix + relative_url, data=data, files=files)
+
+
+def delete(relative_url, data=None):
+    return delete_re(relative_url=url_prefix + relative_url, data=data)
 
 
 class ScaAPI(Sca):
