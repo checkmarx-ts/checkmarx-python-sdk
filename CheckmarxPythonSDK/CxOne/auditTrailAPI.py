ab672d3f208c6d41d2fe6b7d2c006b1fc470c6f4
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2025-08-15 10:20:28 +0800

    [Refactor] audit trail

--- CheckmarxPythonSDK/CxOne/auditTrailAPI.py
+++ CheckmarxPythonSDK/CxOne/auditTrailAPI.py
@@ -32,10 +32,14 @@ def __construct_audit_events(audit_events):
     )
 
 
-def get_audit_events_for_tenant(offset=0, limit=200):
+def get_audit_events_for_tenant(date_from=None, date_to=None, offset=0, limit=200):
     """
-
+    returns a list of audit events for the specified date range. - If no date range is specified, events from the last
+    365 days are returned. - The from date cannot be more than 365 days in the past. - Events for the current day are
+    returned inline. - Events for earlier dates are returned as downloadable links to daily JSON files.
     Args:
+        date_from (str): Start date for the event filter in YYYY-MM-DD format. Cannot be more than 365 days in the past.
+        date_to (str): End date for the event filter in YYYY-MM-DD format. Must be the same or later than the from date.
         offset (int): The number of items to skip before starting to collect the result set
                     Default value : 0
         limit (int):  The number of items to return
@@ -46,7 +50,9 @@ def get_audit_events_for_tenant(offset=0, limit=200):
     """
     type_check(offset, int)
     type_check(limit, int)
-    relative_url = api_url + "?offset={offset}&limit={limit}".format(offset=offset, limit=limit)
+    relative_url = api_url + f"?offset={offset}&limit={limit}"
+    if date_from and date_to:
+        relative_url += f"from={date_from}&to={date_to}"
     response = get_request(relative_url=relative_url)
     item = response.json()
     return __construct_audit_events(audit_events=item)
--- tests/CxOne/test_audit_trail_api.py
+++ tests/CxOne/test_audit_trail_api.py
@@ -4,5 +4,7 @@ from CheckmarxPythonSDK.CxOne import (
 
 
 def test_get_audit_events_for_tenant():
-    result = get_audit_events_for_tenant(limit=1000)
+    result = get_audit_events_for_tenant(limit=1024)
+    assert result is not None
+    result = get_audit_events_for_tenant(date_from='2025-04-22', date_to='2025-08-15', limit=1024)
     assert result is not None
