d05070dde98b566022c9564709a644d32de2fc8c
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2025-11-27 12:59:51 +0800

    [Add] tests\CxOne\test_access_management_api.py::test_create_an_assignment test_delete_an_assignment

--- tests/CxOne/test_access_management_api.py
+++ tests/CxOne/test_access_management_api.py
@@ -76,200 +76,210 @@ from CheckmarxPythonSDK.CxOne.dto import (
     ApplicationsCollection,
     Project,
     ProjectsCollection,
+    EntityType,
+    ResourceType,
 )
 
 
 def test_create_an_assignment():
-    result = create_an_assignment(request_body: AssignmentInput)
+    result = create_an_assignment(
+        assignment_input=AssignmentInput(
+            entity_id="3a7cf5fc-6554-4136-918b-6f494656b2b0",
+            entity_type=EntityType.GROUP,
+            resource_id="71fe66b9-b3ea-4fc7-8594-541d0a07a697",
+            resource_type=ResourceType.TENANT,
+            entity_roles=[],
+        )
+    )
     assert result is not None
 
 
 def test_delete_an_assignment():
-    result = delete_an_assignment(entity_id: str, resource_id: str)
-    assert result is not None
+    result = delete_an_assignment(entity_id="3a7cf5fc-6554-4136-918b-6f494656b2b0", resource_id="71fe66b9-b3ea-4fc7-8594-541d0a07a697")
+    assert result is True
 
 
-def test_retrieve_an_assignment():
-    result = retrieve_an_assignment(entity_id: str, resource_id: str)
-    assert result is not None
+# def test_retrieve_an_assignment():
+#     result = retrieve_an_assignment(entity_id: str, resource_id: str)
+#     assert result is not None
 
 
-def test_update_assignment_roles():
-    result = update_assignment_roles(request_body: EntityRolesRequest, entity_id: str, resource_id: str)
-    assert result is not None
+# def test_update_assignment_roles():
+#     result = update_assignment_roles(request_body: EntityRolesRequest, entity_id: str, resource_id: str)
+#     assert result is not None
 
 
-def test_retrieve_resource_assignments():
-    result = retrieve_resource_assignments(resource_ids: str)
-    assert result is not None
+# def test_retrieve_resource_assignments():
+#     result = retrieve_resource_assignments(resource_ids: str)
+#     assert result is not None
 
 
-def test_create_multiple_assignments():
-    result = create_multiple_assignments(request_body: MultipleAssignmentInput)
-    assert result is not None
+# def test_create_multiple_assignments():
+#     result = create_multiple_assignments(request_body: MultipleAssignmentInput)
+#     assert result is not None
 
 
-def test_add_roles_to_assignment():
-    result = add_roles_to_assignment(request_body: AddAssignmentRoles)
-    assert result is not None
+# def test_add_roles_to_assignment():
+#     result = add_roles_to_assignment(request_body: AddAssignmentRoles)
+#     assert result is not None
 
 
-def test_retrieve_entities():
-    result = retrieve_entities(resource_id: str, resource_type: str, entity_types: str = None)
-    assert result is not None
+# def test_retrieve_entities():
+#     result = retrieve_entities(resource_id: str, resource_type: str, entity_types: str = None)
+#     assert result is not None
 
 
-def test_retrieve_extended_entities_for_resource():
-    result = retrieve_extended_entities_for_resource(resource_id: str, resource_type: str, entity_types: str = None, sort_by: str = None, order: str = None, limit: None = None, offset: None = None, search: str = None)
-    assert result is not None
+# def test_retrieve_extended_entities_for_resource():
+#     result = retrieve_extended_entities_for_resource(resource_id: str, resource_type: str, entity_types: str = None, sort_by: str = None, order: str = None, limit: None = None, offset: None = None, search: str = None)
+#     assert result is not None
 
 
-def test_retrieve_resources():
-    result = retrieve_resources(entity_id: str, resource_types: str = None)
-    assert result is not None
+# def test_retrieve_resources():
+#     result = retrieve_resources(entity_id: str, resource_types: str = None)
+#     assert result is not None
 
 
-def test_check_access():
-    result = check_access(resource_id: str, resource_type: str, action: str)
-    assert result is not None
+# def test_check_access():
+#     result = check_access(resource_id: str, resource_type: str, action: str)
+#     assert result is not None
 
 
-def test_check_access_to_requested_groups():
-    result = check_access_to_requested_groups(group_ids: List[str], project_id: str = None)
-    assert result is not None
+# def test_check_access_to_requested_groups():
+#     result = check_access_to_requested_groups(group_ids: List[str], project_id: str = None)
+#     assert result is not None
 
 
-def test_retrieve_accessible_resources():
-    result = retrieve_accessible_resources(resource_types: str, action: str)
-    assert result is not None
+# def test_retrieve_accessible_resources():
+#     result = retrieve_accessible_resources(resource_types: str, action: str)
+#     assert result is not None
 
 
-def test_retrieve_user's_or_client_groups():
-    result = retrieve_user's_or_client_groups(include_subgroups: bool = None, search: str = None, limit: None = None, offset: None = None)
-    assert result is not None
+# def test_retrieve_user's_or_client_groups():
+#     result = retrieve_user's_or_client_groups(include_subgroups: bool = None, search: str = None, limit: None = None, offset: None = None)
+#     assert result is not None
 
 
-def test_retrieve_user's_or_client_available_groups():
-    result = retrieve_user's_or_client_available_groups(project_id: bool = None, search: str = None, limit: None = None, offset: None = None)
-    assert result is not None
+# def test_retrieve_user's_or_client_available_groups():
+#     result = retrieve_user's_or_client_available_groups(project_id: bool = None, search: str = None, limit: None = None, offset: None = None)
+#     assert result is not None
 
 
-def test_retrieve_groups():
-    result = retrieve_groups(limit: None = None, offset: None = None, search: str = None, ids: str = None)
-    assert result is not None
+# def test_retrieve_groups():
+#     result = retrieve_groups(limit: None = None, offset: None = None, search: str = None, ids: str = None)
+#     assert result is not None
 
 
-def test_retrieve_users():
-    result = retrieve_users(limit: None = None, offset: None = None, search: None = None)
-    assert result is not None
+# def test_retrieve_users():
+#     result = retrieve_users(limit: None = None, offset: None = None, search: None = None)
+#     assert result is not None
 
 
-def test_retrieve_clients():
-    result = retrieve_clients()
-    assert result is not None
+# def test_retrieve_clients():
+#     result = retrieve_clients()
+#     assert result is not None
 
 
-def test_retrieve_users_from_internal_am_storage():
-    result = retrieve_users_from_internal_am_storage()
-    assert result is not None
+# def test_retrieve_users_from_internal_am_storage():
+#     result = retrieve_users_from_internal_am_storage()
+#     assert result is not None
 
 
-def test_retrieve_groups_from_internal_am_storage():
-    result = retrieve_groups_from_internal_am_storage()
-    assert result is not None
+# def test_retrieve_groups_from_internal_am_storage():
+#     result = retrieve_groups_from_internal_am_storage()
+#     assert result is not None
 
 
-def test_retrieve_clients_from_internal_am_storage():
-    result = retrieve_clients_from_internal_am_storage()
-    assert result is not None
+# def test_retrieve_clients_from_internal_am_storage():
+#     result = retrieve_clients_from_internal_am_storage()
+#     assert result is not None
 
 
-def test_retrieve_entity's_base_roles():
-    result = retrieve_entity's_base_roles(entity_id: str)
-    assert result is not None
+# def test_retrieve_entity's_base_roles():
+#     result = retrieve_entity's_base_roles(entity_id: str)
+#     assert result is not None
 
 
-def test_update_base_roles_for_an_entity():
-    result = update_base_roles_for_an_entity(request_body: BaseRolesRequest, entity_id: str)
-    assert result is not None
+# def test_update_base_roles_for_an_entity():
+#     result = update_base_roles_for_an_entity(request_body: BaseRolesRequest, entity_id: str)
+#     assert result is not None
 
 
-def test_assign_base_roles_to_an_entity():
-    result = assign_base_roles_to_an_entity(request_body: BaseRolesRequest, entity_id: str)
-    assert result is not None
+# def test_assign_base_roles_to_an_entity():
+#     result = assign_base_roles_to_an_entity(request_body: BaseRolesRequest, entity_id: str)
+#     assert result is not None
 
 
-def test_delete_base_roles_for_an_entity():
-    result = delete_base_roles_for_an_entity(entity_id: str)
-    assert result is not None
+# def test_delete_base_roles_for_an_entity():
+#     result = delete_base_roles_for_an_entity(entity_id: str)
+#     assert result is not None
 
 
-def test_assign_base_roles_by_role_name():
-    result = assign_base_roles_by_role_name(request_body: BaseRolesRequest, entity_id: str)
-    assert result is not None
+# def test_assign_base_roles_by_role_name():
+#     result = assign_base_roles_by_role_name(request_body: BaseRolesRequest, entity_id: str)
+#     assert result is not None
 
 
-def test_unassign_base_roles_by_role_name():
-    result = unassign_base_roles_by_role_name(request_body: BaseRolesRequest, entity_id: str)
-    assert result is not None
+# def test_unassign_base_roles_by_role_name():
+#     result = unassign_base_roles_by_role_name(request_body: BaseRolesRequest, entity_id: str)
+#     assert result is not None
 
 
-def test_retrieve_roles():
-    result = retrieve_roles()
-    assert result is not None
+# def test_retrieve_roles():
+#     result = retrieve_roles()
+#     assert result is not None
 
 
-def test_create_a_role():
-    result = create_a_role(request_body: CreateRoleRequest)
-    assert result is not None
+# def test_create_a_role():
+#     result = create_a_role(request_body: CreateRoleRequest)
+#     assert result is not None
 
 
-def test_retrieve_role():
-    result = retrieve_role(role_id: str)
-    assert result is not None
+# def test_retrieve_role():
+#     result = retrieve_role(role_id: str)
+#     assert result is not None
 
 
-def test_update_a_role():
-    result = update_a_role(request_body: CreateRoleRequest, role_id: str)
-    assert result is not None
+# def test_update_a_role():
+#     result = update_a_role(request_body: CreateRoleRequest, role_id: str)
+#     assert result is not None
 
 
-def test_delete_a_role():
-    result = delete_a_role(role_id: str)
-    assert result is not None
+# def test_delete_a_role():
+#     result = delete_a_role(role_id: str)
+#     assert result is not None
 
 
-def test_retrieve_permissions():
-    result = retrieve_permissions()
-    assert result is not None
+# def test_retrieve_permissions():
+#     result = retrieve_permissions()
+#     assert result is not None
 
 
-def test_retrieve_users_with_resources():
-    result = retrieve_users_with_resources(search: str = None, base_roles: str = None, username: str = None, empty_assignments: bool = None, no_groups: bool = None, created_from: str = None, created_to: str = None, sort_by: str = None, order: str = None, limit: None = None, offset: None = None)
-    assert result is not None
+# def test_retrieve_users_with_resources():
+#     result = retrieve_users_with_resources(search: str = None, base_roles: str = None, username: str = None, empty_assignments: bool = None, no_groups: bool = None, created_from: str = None, created_to: str = None, sort_by: str = None, order: str = None, limit: None = None, offset: None = None)
+#     assert result is not None
 
 
-def test_retrieve_clients_with_resources():
-    result = retrieve_clients_with_resources(search: str = None, base_roles: str = None, client_id: str = None, empty_assignments: bool = None, sort_by: str = None, order: str = None, limit: None = None, offset: None = None, no_groups: bool = None, created_from: str = None, created_to: str = None)
-    assert result is not None
+# def test_retrieve_clients_with_resources():
+#     result = retrieve_clients_with_resources(search: str = None, base_roles: str = None, client_id: str = None, empty_assignments: bool = None, sort_by: str = None, order: str = None, limit: None = None, offset: None = None, no_groups: bool = None, created_from: str = None, created_to: str = None)
+#     assert result is not None
 
 
-def test_retrieve_groups_with_resources():
-    result = retrieve_groups_with_resources(search: str = None, base_roles: str = None, name: str = None, empty_assignments: bool = None, no_members: bool = None, sort_by: str = None, created_from: str = None, created_to: str = None, order: str = None, limit: None = None, offset: None = None)
-    assert result is not None
+# def test_retrieve_groups_with_resources():
+#     result = retrieve_groups_with_resources(search: str = None, base_roles: str = None, name: str = None, empty_assignments: bool = None, no_members: bool = None, sort_by: str = None, created_from: str = None, created_to: str = None, order: str = None, limit: None = None, offset: None = None)
+#     assert result is not None
 
 
-def test_get_a_list_of_permissions_of_entity_for_resource():
-    result = get_a_list_of_permissions_of_entity_for_resource(entity_id: str, entity_type: str, resource_id: str = None, resource_type: str)
-    assert result is not None
+# def test_get_a_list_of_permissions_of_entity_for_resource():
+#     result = get_a_list_of_permissions_of_entity_for_resource(entity_id: str, entity_type: str, resource_id: str = None, resource_type: str)
+#     assert result is not None
 
 
-def test_get_a_list_of_applications_with_action_for_user/client():
-    result = get_a_list_of_applications_with_action_for_user/client(action: str, offset: int = None, limit: int = None, name: str = None, tags_keys: List[str] = None, tags_values: List[str] = None)
-    assert result is not None
+# def test_get_a_list_of_applications_with_action_for_user/client():
+#     result = get_a_list_of_applications_with_action_for_user/client(action: str, offset: int = None, limit: int = None, name: str = None, tags_keys: List[str] = None, tags_values: List[str] = None)
+#     assert result is not None
 
 
-def test_get_a_list_of_projects_with_action_for_user/client():
-    result = get_a_list_of_projects_with_action_for_user/client(action: str, offset: int = None, limit: int = None, name: str = None, tags_keys: List[str] = None, tags_values: List[str] = None)
-    assert result is not None
+# def test_get_a_list_of_projects_with_action_for_user/client():
+#     result = get_a_list_of_projects_with_action_for_user/client(action: str, offset: int = None, limit: int = None, name: str = None, tags_keys: List[str] = None, tags_values: List[str] = None)
+#     assert result is not None
 
