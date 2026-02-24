205a9e1a0409c5e24a7553e90d276cf9ea40a33d
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2026-02-24 10:53:53 +0800

    test_clients_api

--- tests/CxOne/KeycloakAPI/test_clients_api.py
+++ tests/CxOne/KeycloakAPI/test_clients_api.py
@@ -1,16 +1,11 @@
 from CheckmarxPythonSDK.CxOne.KeycloakAPI import (
-    get_all_oauth_clients,
-    get_oauth_client_by_name,
-    get_all_oauth_client_by_id,
-    create_oauth_client,
-    edit_auth_client,
-    get_oauth_service_account_user,
-    add_group_to_oauth_client,
-    generate_oauth_secret,
-    get_client_ast_app,
+    ClientsApi,
 )
-
+from pprint import pprint
 
 def test_get_client_ast_app():
-    client_ast_app = get_client_ast_app(realm="happy")
-    assert client_ast_app is not None
+    clients = ClientsApi().get_clients(realm="happy")
+    print(f"number of clients: {len(clients)}")
+    for client in clients:
+        pprint(client)
+    assert clients is not None
